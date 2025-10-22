c7aa9fb6b8505e7705411fec3fe675f7342fe9bb
Guilherme jeske <guilhermeieski@gmail.com>
2025-10-22 10:36:01 -0300

    altera ome de classe

--- app/Models/PessoaEndereco.php
+++ app/Models/PessoaEndereco.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UsuarioEndereco extends Model
+class PessoaEndereco extends Model
 {
     protected $table = 'enderecos_pessoas';
 
